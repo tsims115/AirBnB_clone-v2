7242f7fd071dd2b776ed9a52922996166cd0c33e
tsims115 <tsims115@yahoo.com>
2022-03-17 11:44:46 -0700

    Adding all tasks and readme

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module 1-hbnb_route.py with app route '/' and '/hbnb'"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """returns hello HBNB!"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbhn_route():
+    """returns HBNB"""
+    return 'HBNB'
+
+if __name__ == "__main__":
+    """app.debug = True"""
+    app.run(host = '0.0.0.0', port = 5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module 1-hbnb_route.py with app route '/' and '/hbnb'"""
+from flask import Flask
+from flask import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """returns hello HBNB!"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbhn_route():
+    """returns HBNB"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Displays c <text>"""
+    ntext = escape(text).replace("_", " ")
+    return 'c %s' % ntext
+
+if __name__ == "__main__":
+    """app.debug = True"""
+    app.run(host = '0.0.0.0', port = 5000)
